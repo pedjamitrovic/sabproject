9535b39e64ce286434f85bb244db98664bc9494b
pedjamitrovic <pre11mit@yahoo.com>
2019-05-28 15:24:24 +0200

    ArticleOperations done

--- OnlineStore/src/student/mp150608_ArticleOperations.java
+++ OnlineStore/src/student/mp150608_ArticleOperations.java
@@ -2,9 +2,31 @@ package student;
 
 import operations.ArticleOperations;
 
+import java.sql.*;
+
 public class mp150608_ArticleOperations implements ArticleOperations {
     @Override
     public int createArticle(int shopId, String articleName, int articlePrice) {
-        return 0;
+        try (Connection c = DriverManager.getConnection(Settings.connectionUrl)){
+            PreparedStatement ps = c.prepareStatement("select * from ARTICLE where NAME = ?");
+            ps.setString(1, articleName);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) return -1;
+
+            String[] generatedColumns = { "ID" };
+            ps = c.prepareStatement("insert into ARTICLE values(?, ?, ?, ?)", generatedColumns);
+            ps.setString(1, articleName);
+            ps.setInt(2, articlePrice);
+            ps.setFloat(3, 0);
+            ps.setInt(4, shopId);
+            if (ps.executeUpdate() == 0) return -1;
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) return generatedKeys.getInt("ID");
+                else return -1;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
